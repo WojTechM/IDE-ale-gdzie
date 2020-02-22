f38d70cb75d10c68e0f625605ee20bd8875eb610
Wojciech Makieła <wojciech.m1997@gmail.com>
2020-02-22 15:26:44 +0100

    Stworzenie struktury katalogów oraz wstępnej wersji spisu treści

---
+++ src/main/java/com/github/wojtechm/ide/Start.java
@@ -0,0 +1,27 @@
+package com.github.wojtechm.ide;
+
+/**
+ * W tym repozytorium znajdziesz informacje o nawigacji oraz skrótach klawiszowych wewnątrz IntelliJ IDEA.
+ * Część wskazówek jest wspólna z innymi IDE.
+ *
+ * Spis treści:
+ * I. Poruszanie w Edytorze Tekstu
+ * - Poruszanie kursorem
+ * - Przeskakiwanie między otwartymi plikami
+ * - Otwieranie i zamykanie plików
+ *
+ * II. Nawigacja Między Okienkami IntelliJ oraz Ich Obsługa
+ * - Jak stworzyć nowy interfejs
+ * - Jak dostać się do ustawień i je zmienić
+ *
+ * III. Edycja Tekstu
+ * - Sposoby na zaznaczenie tekstu
+ * - Podstawowe operacje na tekście
+ *
+ * IV. Refaktoryzacja Kodu
+ * - Uprzątnięce kodu w pliku/projekcie
+ * - Wyciągnięcie fragmentu kodu do osobnej metody
+ * -
+ */
+public class Start {
+}
