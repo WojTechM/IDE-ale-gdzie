f3b6eb5c0e9ba42198855350d3766e265c3f8028
Wojciech Makieła <wojciech.m1997@gmail.com>
2020-02-24 13:28:57 +0100

    Zmiana kolejności wskazówek w spisie treści oraz dodanie informacji o zawijaniu tekstu

--- src/main/java/com/github/wojtechm/ide/Start.java
+++ src/main/java/com/github/wojtechm/ide/Start.java
@@ -5,28 +5,40 @@ package com.github.wojtechm.ide;
  * Część wskazówek jest kompatybilna z innymi IDE.
  *
  * Spis treści:
- * I. Poruszanie w Edytorze Tekstu
+ * S. {@link Start}
+ * - Podstawowe informacje przydane podczas pracy z tym repozytorium.
+ *
+ * I. Edycja Tekstu
  * - Poruszanie kursorem
- * - Przeskakiwanie między otwartymi plikami
- * - Otwieranie i zamykanie plików
- *
- * II. Nawigacja Między Okienkami IntelliJ oraz Ich Obsługa
- * - Jak stworzyć nowy interfejs
- * - Jak dostać się do ustawień i je zmienić
- *
- * III. Edycja Tekstu
  * - Sposoby na zaznaczenie tekstu
  * - Podstawowe operacje na tekście
  *
- * IV. Refaktoryzacja Kodu
+ * II. Refaktoryzacja Kodu
  * - Uprzątnięce kodu w pliku/projekcie
  * - Wyciągnięcie fragmentu kodu do osobnej metody
  *
+ * III. Nawigacja Między Okienkami IntelliJ oraz Ich Obsługa
+ * - Otwieranie i zamykanie plików
+ * - Przeskakiwanie między otwartymi plikami
+ * - Jak stworzyć nowy interfejs
+ * - Jak dostać się do ustawień i je zmienić
  *
- * WAŻNE! W tym repozytorium dokumentacja, komentarze, nazwy klas, metod i pól będą w języku polskim. Co za tym idzie -
- * twoje IDE będzie podkreślać większość nazw. Aby temu zaradzić skorzystaj ze skrótu klawiszowego
- * ~Ctrl + Shift + Alt + H~ i zmniejsz "Highlighting Level" do wartości "None".
- *
+ * @author Wojciech Makieła (github.com/WojTechM)
  */
-public class Start {
+class Start {
+
+    private class Podstawy {
+        /**
+         * 1. W tym repozytorium dokumentacja, komentarze, nazwy klas, metod i pól będą w języku polskim. Co za tym
+         * idzie - twoje IDE będzie podkreślać większość nazw. Aby temu zaradzić skorzystaj ze skrótu klawiszowego
+         * ~Ctrl + Shift + Alt + H~ i zmniejsz "Highlighting Level" do wartości "None".
+         *
+         * 2. Aby 'rozwinąć' blok tekstu użyj ~Ctrl + PLUS~. Aby zwinąć blok, użyj ~Ctrl + MINUS~
+         * Przetestuj proszę oba skróty. Umieść kursor gdzieś w obrębie tego bloku tekstu i klikaj.
+         * ~Ctrl + Shift + MINUS~ oraz ~Ctrl + Shift + PLUS~ zadziałają na wszystkie bloki w pliku.
+         * Ostatni jest ~Ctrl + .~ który może być wykorzystany zarówno do zwijania, jak i rozwijania tekstu.
+         * Sugestia dotycząca pracy z tym repozytorium - po przejściu do nowego pliku zwiń wszystkie bloki i kolejno
+         * rozwijaj je w miarę czytania.
+         */
+    }
 }
