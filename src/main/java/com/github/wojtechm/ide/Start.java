a11ea2565c29b2ac97751f4aeecea9cbda870b6a
WojTechM <wojciech.m197@gmail.com>
2020-07-12 11:36:08 +0200

    Rozszerzenie dokumentacji o znaczniki HTML

--- src/main/java/com/github/wojtechm/ide/Start.java
+++ src/main/java/com/github/wojtechm/ide/Start.java
@@ -3,42 +3,58 @@ package com.github.wojtechm.ide;
 /**
  * W tym repozytorium znajdziesz informacje o nawigacji oraz skrótach klawiszowych wewnątrz IntelliJ IDEA.
  * Część wskazówek jest kompatybilna z innymi IDE.
- *
+ * <p>
+ * <p>
  * Spis treści:
- * S. {@link Start}
- * - Podstawowe informacje przydane podczas pracy z tym repozytorium.
+ * <ol>
+ *      <li>{@link Start}
+ *          <ul>
+ *              <li>Podstawowe informacje przydane podczas pracy z tym repozytorium</li>
+ *          </ul>
+ *      </li>
  *
- * I. Edycja Tekstu
- * - Poruszanie kursorem
- * - Sposoby na zaznaczenie tekstu
- * - Podstawowe operacje na tekście
+ *      <li>Edycja Tekstu
+ *          <ul>
+ *               <li>Poruszanie kursorem</li>
+ *               <li>Sposoby na zaznaczenie tekstu</li>
+ *               <li>Podstawowe operacje na tekście</li>
+ *          </ul>
+ *      </li>
+ *      <li>Refaktoryzacja Kodu
+ *          <ul>
+ *               <li>Uprzątnięce kodu w pliku/projekcie</li>
+ *               <li>Wyciągnięcie fragmentu kodu do osobnej metody</li>
+ *          </ul>
+ *      </li>
+ *      <li>Nawigacja Między Okienkami IntelliJ oraz Ich Obsługa
+ *          <ul>
+ *               <li>Otwieranie i zamykanie plików</li>
+ *               <li>Przeskakiwanie między otwartymi plikami</li>
+ *               <li>Jak stworzyć nowy interfejs</li>
+ *               <li>Jak dostać się do ustawień i je zmienić</li>
+ *          </ul>
+ *      </li>
+ * </ol>
+ * <p>
  *
- * II. Refaktoryzacja Kodu
- * - Uprzątnięce kodu w pliku/projekcie
- * - Wyciągnięcie fragmentu kodu do osobnej metody
- *
- * III. Nawigacja Między Okienkami IntelliJ oraz Ich Obsługa
- * - Otwieranie i zamykanie plików
- * - Przeskakiwanie między otwartymi plikami
- * - Jak stworzyć nowy interfejs
- * - Jak dostać się do ustawień i je zmienić
- *
- * @author Wojciech Makieła (github.com/WojTechM)
+ * @author Wojciech Makieła
  */
 class Start {
+    /*
+    1. W tym repozytorium dokumentacja, komentarze, nazwy klas, metod i pól będą w języku polskim. Co za tym
+    idzie - twoje IDE będzie podkreślać większość nazw. Aby temu zaradzić skorzystaj ze skrótu klawiszowego
+    ~Ctrl + Shift + Alt + H~ i zmniejsz "Highlighting Level" do wartości "None".
+     */
 
-    private class Podstawy {
-        /**
-         * 1. W tym repozytorium dokumentacja, komentarze, nazwy klas, metod i pól będą w języku polskim. Co za tym
-         * idzie - twoje IDE będzie podkreślać większość nazw. Aby temu zaradzić skorzystaj ze skrótu klawiszowego
-         * ~Ctrl + Shift + Alt + H~ i zmniejsz "Highlighting Level" do wartości "None".
-         *
-         * 2. Aby 'rozwinąć' blok tekstu użyj ~Ctrl + PLUS~. Aby zwinąć blok, użyj ~Ctrl + MINUS~
-         * Przetestuj proszę oba skróty. Umieść kursor gdzieś w obrębie tego bloku tekstu i klikaj.
-         * ~Ctrl + Shift + MINUS~ oraz ~Ctrl + Shift + PLUS~ zadziałają na wszystkie bloki w pliku.
-         * Ostatni jest ~Ctrl + .~ który może być wykorzystany zarówno do zwijania, jak i rozwijania tekstu.
-         * Sugestia dotycząca pracy z tym repozytorium - po przejściu do nowego pliku zwiń wszystkie bloki i kolejno
-         * rozwijaj je w miarę czytania.
-         */
-    }
+    /*
+    2. Aby 'rozwinąć' blok tekstu użyj ~Ctrl + PLUS~. Aby zwinąć blok, użyj ~Ctrl + MINUS~
+    Przetestuj proszę oba skróty. Umieść kursor gdzieś w obrębie tego bloku tekstu i klikaj.
+    */
+
+    /*
+    ~Ctrl + Shift + MINUS~ oraz ~Ctrl + Shift + PLUS~ zadziałają na wszystkie bloki w pliku.
+    Ostatni jest ~Ctrl + .~ który może być wykorzystany zarówno do zwijania, jak i rozwijania tekstu.
+    Sugestia dotycząca pracy z tym repozytorium - po przejściu do nowego pliku zwiń wszystkie bloki i kolejno
+    rozwijaj je w miarę czytania.
+     */
 }
